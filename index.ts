f64d697457cb2374a833406f92e858e8ce800907
Asadullah109 <147506148+Asadullah109@users.noreply.github.com>
2025-01-09 20:17:14 +0500

    w

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import ContactFormScehma from "./contact-form";
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [ContactFormScehma],
+};
+
+
